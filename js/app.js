6e99f8bd6cd9a62c81e573f75a73f5d6598db145
Jordan-Burgess <alfredjordan93@gmail.com>
2022-08-08 14:07:22 -0400

    Create a card and deck class. Create a deck instance with 52 cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+class Card {
+    constructor(suit, rank, faceUp, select){
+        this.suit = suit;
+        this.rank = rank;
+        this.faceUp = faceUp;
+        this.select = select;
+    }
+}
+
+class Deck {
+    constructor (length, cards){
+        this.length = length;
+        this.cards = cards;
+    }
+    draw(){
+        console.log(cards)
+    }
+}
+
+const suit = ['H', 'S', 'C', 'D',];
+const rank = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K',];
+let solitaireDeck = new Deck(52, []);
+
+for (let i=0; i<suit.length; i++){
+    for (let j=0; j<rank.length; j++){
+        let card = new Card(suit[i], rank[j], false, false);
+        solitaireDeck.cards.push(card);
+    }
+}
+
